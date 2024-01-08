089b2b8aa48a9214b80cf203495213dd659e96e6
kongehund <63306696+kongehund@users.noreply.github.com>
2024-01-09 00:33:33 +0100

    Add Clear All confirmation dialog

--- Classes/Visual/ClearAllButton.cs
+++ Classes/Visual/ClearAllButton.cs
@@ -4,13 +4,22 @@ using System;
 
 public partial class ClearAllButton : Button
 {
+    [Export]
+    private ConfirmationDialog confirmationDialog;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
         Pressed += OnPressed;
+        confirmationDialog.Confirmed += ClearAllTimingPoints;
 	}
 
     private void OnPressed()
+    {
+        confirmationDialog.Popup();
+    }
+
+    private void ClearAllTimingPoints()
     {
         Timing.Instance.TimingPoints.Clear();
         Timing.Instance.TimeSignaturePoints.Clear();
