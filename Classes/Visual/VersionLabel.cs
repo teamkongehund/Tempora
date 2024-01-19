bb5671ffafda67b0bdf2cfae9d647855b244363a
kongehund <63306696+kongehund@users.noreply.github.com>
2024-01-19 23:44:59 +0100

    Add version label

--- Classes/Visual/VersionLabel.cs
+++ Classes/Visual/VersionLabel.cs
@@ -1,15 +1,12 @@
 using Godot;
-using System;
+
+namespace OsuTimer.Classes.Visual;
 
 public partial class VersionLabel : Label
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+        Text = ThisAssembly.Git.Tag;
 	}
 }
