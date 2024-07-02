1ad3a51f909aa4e1c38718698cfae1aeeb4ecf7e
kongehund <63306696+kongehund@users.noreply.github.com>
2024-07-02 14:02:39 +0200

    Update grid label

--- Classes/Visual/Scrollbars/GridScrollBar.cs
+++ Classes/Visual/Scrollbars/GridScrollBar.cs
@@ -1,12 +1,27 @@
-using Godot;
-using Tempora.Classes.Audio;
+using System.Collections.Generic;
 using Tempora.Classes.Utility;
 
 namespace Tempora.Classes.Visual;
 
 public partial class GridScrollBar : LabeledScrollbarHorizontal
 {
-    protected override void UpdateValueLabel() => valueLabel.Text = Settings.GridSliderToDivisorDict[(int)hScrollBar.Value].ToString();
+    protected static readonly Dictionary<int, string> GridDivisorToLabelDict = new() {
+        { 1, "4/4" },
+        { 2, "2/4" },
+        { 3, "1/3" },
+        { 4, "1/4" },
+        { 6, "1/6" },
+        { 8, "1/8" },
+        { 12, "1/12" },
+        { 16, "1/16" }
+    };
+
+    protected override void UpdateValueLabel()
+    {
+        var divisor = Settings.GridSliderToDivisorDict[(int)hScrollBar.Value];
+        var label = GridDivisorToLabelDict[divisor];
+        valueLabel.Text = label;
+    }
 
     protected override void UpdateTarget() => Settings.Instance.GridDivisor = Settings.GridSliderToDivisorDict[(int)hScrollBar.Value];
 
