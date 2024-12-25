e4a31132ffdd70fadcf391dae67044fcd24b7427
kongehund <63306696+kongehund@users.noreply.github.com>
2024-12-25 18:06:44 +0100

    add spaces for readability

--- Classes/TimingClasses/TimingEvents.cs
+++ Classes/TimingClasses/TimingEvents.cs
@@ -222,26 +222,38 @@ public partial class Timing
                 if (previousPointBlocksChange || nextPointBlockschange)
                     return false;
                 return true;
+
             case TimingPoint.PropertyType.TimeSignature:
                 return true;
+
             case TimingPoint.PropertyType.MeasuresPerSecond:
                 if (nextTimingPoint == null)
                     return true;
+
                 float? calculatedMPS = CalculateMPSBasedOnAdjacentPoints(timingPoint);
+
                 return (float)newValue! == calculatedMPS!;
+
             case TimingPoint.PropertyType.Bpm:
                 if (nextTimingPoint == null)
                     return true;
+
                 calculatedMPS = CalculateMPSBasedOnAdjacentPoints(timingPoint);
+
                 if (calculatedMPS == null)
                     return true;
+
                 float newBpm = (float)newValue!;
                 float calculatedBpm = timingPoint.MpsToBpm((float)calculatedMPS);
+
                 if (Math.Abs(calculatedBpm - newBpm) < 0.0001)
                     return true;
+
                 return false;
+
             case TimingPoint.PropertyType.MusicPosition:
                 return CanTimingPointGoHere(timingPoint, (float)newValue!, out _);
+
             default:
                 return false;
         }
