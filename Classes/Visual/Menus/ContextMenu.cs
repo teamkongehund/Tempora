ac781b6c3dcb511c5d22f7f88e3fe35f0ea3f104
kongehund <63306696+kongehund@users.noreply.github.com>
2025-01-01 14:32:31 +0100

    Fix broken dependencies

--- Classes/Visual/Menus/ContextMenu.cs
+++ Classes/Visual/Menus/ContextMenu.cs
@@ -2,12 +2,12 @@
 // 
 // This file is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0).
 // You are free to:
-// - Share — copy and redistribute the material in any medium or format
+// - Share, copy and redistribute the material in any medium or format
 //
 // Under the following terms:
-// - Attribution — You must give appropriate credit, provide a link to the license, and indicate if changes were made.
-// - NonCommercial — You may not use the material for commercial purposes.
-// - NoDerivatives — If you remix, transform, or build upon the material, you may not distribute the modified material.
+// - Attribution - You must give appropriate credit, provide a link to the license, and indicate if changes were made.
+// - NonCommercial - You may not use the material for commercial purposes.
+// - NoDerivatives - If you remix, transform, or build upon the material, you may not distribute the modified material.
 //
 // Full license text is available at: https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
 
